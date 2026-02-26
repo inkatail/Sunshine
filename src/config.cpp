916875076b87b86f6e7c0e1b9695e7aa003e0a20
David Lane <42013603+ReenigneArcher@users.noreply.github.com>
2026-02-26 09:44:49 -0500

    Apply suggestion from @ReenigneArcher

--- src/config.cpp
+++ src/config.cpp
@@ -1116,7 +1116,7 @@ namespace config {
     int_f(vars, "vt_realtime", video.vt.vt_realtime, vt::rt_from_view);
 
     bool_f(vars, "vaapi_strict_rc_buffer", video.vaapi.strict_rc_buffer);
-    int_f(vars, "vaapi_async_depth", video.vaapi.async_depth);;
+    int_f(vars, "vaapi_async_depth", video.vaapi.async_depth);
 
     string_f(vars, "capture", video.capture);
     string_f(vars, "encoder", video.encoder);
